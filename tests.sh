97c0c1a45365b34fcdfce719fe1e340b6e16c157
tulios <ornelas.tulio@gmail.com>
2012-01-29 12:37:46 -0200

    improving temporaly sh to run tests

--- tests.sh
+++ tests.sh
@@ -4,6 +4,13 @@ echo ""
 echo "Compiling..."
 dmd -unittest src/dmdspec.d src/dsl.d src/matchers.d test/null_test.d test/bool_test.d test/dsl_test.d test/int_test.d test/string_test.d test/main.d
 
-echo ""
-echo "Running... "
-./dmdspec
+OUT=$?
+
+if [ $OUT -eq 0 ];then
+	echo ""
+	echo "Running... "
+	./dmdspec
+else
+	echo ""
+	echo "Build Fail!"
+fi
