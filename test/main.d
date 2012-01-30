9e2bf740d1f839ad92b1f58ea5a7b940cfb304bd
Bruno Torres <lurehendi@gmail.com>
2012-01-30 14:20:08 -0200

    Changing dsl syntax to use the assignment operator instead of passing blocks as function parameter calls.

--- test/main.d
+++ test/main.d
@@ -3,59 +3,59 @@ import dmdspec;
 import std.exception;
 
 void main() {
-	describe("Matchers", {
-		describe("using beTrue", {
-			it("should be true when is true", {
+	describe("Matchers") = {
+		describe("using beTrue") = {
+			it("should be true when is true") = {
 				subject(true).should(beTrue());
-			});
-			it("should not be true when is false", {
+			};
+			it("should not be true when is false") = {
 				subject(false).shouldNot(beTrue());
-			});
-		});
-		describe("using beFalse", {
-			it("should be false when is false", {
+			};
+		};
+		describe("using beFalse") = {
+			it("should be false when is false") = {
 				subject(false).should(beFalse());
-			});
-			it("should not be false when is true", {
+			};
+			it("should not be false when is true") = {
 				subject(true).shouldNot(beFalse());
-			});
-		});
-		describe("using beEqual", {
-			describe("with numbers", {
-				it("should be equal to when is the same number", {
+			};
+		};
+		describe("using beEqual") = {
+			describe("with numbers") = {
+				it("should be equal to when is the same number") = {
 					subject(1).should(beEqualTo(1));
-				});
-				it("should not be equal when is a different number", {
+				};
+				it("should not be equal when is a different number") = {
 					subject(1).shouldNot(beEqualTo(2));
-				});
-			});
+				};
+			};
 			
-			describe("with strings", {
-				it("should be equal to when is the same number", {
+			describe("with strings") = {
+				it("should be equal to when is the same number") = {
 					subject("string").should(beEqualTo("string"));
-				});
-				it("should not be equal when is a different number", {
+				};
+				it("should not be equal when is a different number") = {
 					subject("string").shouldNot(beEqualTo("another string"));
-				});
-			});
-		});
-	});
+				};
+			};
+		};
+	};
 
-	describe("Integer numbers", {
-		describe("number 1", {
-			it("should be equal to 1", {
+	describe("Integer numbers") = {
+		describe("number 1") = {
+			it("should be equal to 1") = {
 				subject(1).should(beEqualTo(1));
-			});
-			it("should not be equal to 2", {
+			};
+			it("should not be equal to 2") = {
 				subject(1).shouldNot(beEqualTo(2));
-			});
-		});
-		describe("divided by 0", {
-			it("should throw an DividedByZeroException", {
+			};
+		};
+		describe("divided by 0") = {
+			it("should throw an DividedByZeroException") = {
 				subject( { div( 1, 0 ); } ).should( throwAnException( "main.DividedByZeroException" ) );
-			});
-		});
-	});
+			};
+		};
+	};
 }
 
 class DividedByZeroException : Exception
