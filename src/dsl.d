a791f5b08e2216fb3a39cabad6b79b8f848b6329
Bruno Torres <lurehendi@gmail.com>
2012-01-30 14:20:08 -0200

    Alterando sintaxe da dsl para usar o operador de atribuição

--- src/dsl.d
+++ src/dsl.d
@@ -10,23 +10,54 @@ Subject!(T) subject(T)(lazy T value) {
 	return new Subject!(T)(value);
 }
 
-void describe(string description, void delegate() intention) {
-	Reporter.report(description, intention);
+class BaseSpec {
+	private string description;
+	
+	this(string description) {
+		this.description = description;
+	}
 }
 
-void it(string description, void delegate() intention) {
-	ExampleResult example = new ExampleResult(description);
-	
-	try {	
-		intention();
-		example.setStatus(ExampleResult.STATUS.SUCCESS);
-		
-	} catch (SpecFailureException e) {
-		example.setStatus(ExampleResult.STATUS.FAIL);
-		example.setExpectation(e.expectation);
-		example.setGot(e.got);
-		example.pushToStack(to!(string)(e.file) ~ ":" ~ to!(string)(e.line));
-	}
-	
-	Reporter.report(example);
+T spec(T)(string description) {
+	return new T(description);
 }
+
+class Describe : BaseSpec {
+	this(string description) {
+		super(description);
+	}
+
+	Describe opAssign(void delegate() intention) {
+		Reporter.report(description, intention);
+		return this;
+	}
+}
+
+alias spec!(Describe) describe;
+
+class It : BaseSpec {
+	this(string description) {
+		super(description);
+	}
+
+	It opAssign(void delegate() intention) {
+ 		ExampleResult example = new ExampleResult(description);
+	
+		try {	
+			intention();
+			example.setStatus(ExampleResult.STATUS.SUCCESS);
+		
+		} catch (SpecFailureException e) {
+			example.setStatus(ExampleResult.STATUS.FAIL);
+			example.setExpectation(e.expectation);
+			example.setGot(e.got);
+			example.pushToStack(to!(string)(e.file) ~ ":" ~ to!(string)(e.line));
+		}
+	
+		Reporter.report(example);
+		return this;
+	}
+}
+
+alias spec!(It) it;
+
