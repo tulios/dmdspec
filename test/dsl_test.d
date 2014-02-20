eac23af635c1184480e8b2c3cf4fc7a689f3fd5f
Bruno Torres <bruno.torres@me.com>
2014-02-20 15:39:44 -0300

    Fixed class info matching on dsl tests.

--- test/dsl_test.d
+++ test/dsl_test.d
@@ -3,15 +3,15 @@ import dmdspec;
 unittest {
 	// Subject
 	assert(
-		subject(1).classinfo.toString() == "dmdspec.Subject!(int).Subject", 
-		"method subject should instantiate an object of type Subject!(int) when receive an int value"
+		subject(1).classinfo.toString() == "dmdspec.Subject!int.Subject",
+		"method subject should instantiate an object of type Subject!int when receive an int value"
 	);
 	assert(
-		subject("string").classinfo.toString() == "dmdspec.Subject!(string).Subject", 
-		"method subject should instantiate an object of type Subject!(string) when receive a string value"
+		subject("string").classinfo.toString() == "dmdspec.Subject!string.Subject",
+		"method subject should instantiate an object of type Subject!string when receive a string value"
 	);
 	assert(
-		subject(null).classinfo.toString() == "dmdspec.Subject!(typeof(null)).Subject", 
+		subject(null).classinfo.toString() == "dmdspec.Subject!(typeof(null)).Subject",
 		"method subject should instantiate an object of type Subject!(typeof(null)) when receive a null value"
 	);
 	
